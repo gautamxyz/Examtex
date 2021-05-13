1a4d3f0c9aef977d098717cc579ca3851f11e2ce
gautamxyz <83075538+gautamxyz@users.noreply.github.com>
2021-05-13 12:11:35 +0530

    Update ast.h

--- ast.h
+++ ast.h
@@ -6,29 +6,29 @@
 #include <string.h>
 
 float DQ[2], DF[2];
-int NUMQ;
-int NUMF;
+int NUMQ; // number of mcqs user wants in each question paper
+int NUMF; // number of fill in the blanks user wants in each question paper
 
 
-int  NUMMCQ ;
-int  NUMFILL ;
+int  NUMMCQ ; // total number of mcqs in question bank
+int  NUMFILL ; // total number of fill in the blanks in question bank
 
 typedef struct TreeNode Node;
 typedef Node *PtrToNode;
 
 struct TreeNode
 {
-    char Array[300];
+    char Array[500];
     PtrToNode NextSibling;
     PtrToNode FirstChild;
 };
 
-void Parseqp(FILE *q);
-PtrToNode MakeNode();
-PtrToNode InitTree();
-void MakePtrsToQues(int n, PtrToNode MCQArray[], int m, PtrToNode FILLArray[]);
-PtrToNode ParseBank(FILE *fp);
-void MakeQuesPaperFile(PtrToNode Root);
-int getkey(int , int);
+void Parseqp(FILE *q); // parses qp.txt
+PtrToNode MakeNode(); // Creates and initialises a node
+PtrToNode InitTree(); // Initialises tree (ast)
+void MakePtrsToQues(int n, PtrToNode MCQArray[], int m, PtrToNode FILLArray[]); // makes pointers to questions
+PtrToNode ParseBank(FILE *fp); // parses question bank into ast
+void MakeQuesPaperFile(PtrToNode Root); // outputs 3 question papers in .md format
+int getkey(int , int); // gets key
 
 #endif
